4aab2d967c702fad7df21a6dcea4a97491e5a815
이예서 <49704047+yeseoLee@users.noreply.github.com>
2025-02-05 01:53:27 +0900

    fix: admin 기능 제거

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from database import engine
 from fastapi import FastAPI
 import models
 from novel import novel_router
-from novel.admin_router import admin_router
+
+# from novel.admin_router import admin_router
 from starlette.middleware.cors import CORSMiddleware
 from user import user_router
 from user.user_router import active_router
@@ -13,13 +14,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight.theme": "obsidian"})
 
-app.include_router(user_router.app, tags=["user"])
-app.include_router(novel_router.app, tags=["novel"])
-app.include_router(comment_router.app, tags=["comment"])
-app.include_router(active_router, prefix="/user", tags=["user"])
-app.include_router(admin_router, prefix="/api", tags=["admin"])
-
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -28,7 +22,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.include_router(user_router.app, tags=["user"])
+app.include_router(novel_router.app, tags=["novel"])
+app.include_router(comment_router.app, tags=["comment"])
+app.include_router(active_router, prefix="/user", tags=["user"])
+# app.include_router(admin_router, tags=["admin"])
+
 
 @app.get("/")
-def health_check():
+async def health_check():
     return {"version": "1.0.0"}
