bbd38433113870ecacf2047e1ae12beca1a2734a
이예서 <49704047+yeseoLee@users.noreply.github.com>
2025-02-09 14:56:30 +0900

    fix: 댓글 작성 오류 수정

--- comment/comment_schema.py
+++ comment/comment_schema.py
@@ -8,6 +8,16 @@ class CommentCreate(BaseModel):
     novel_shorts_no: int
     parent_no: Optional[int] = None
     content: str
+    user_no: Optional[int] = None
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "novel_shorts_no": 1,
+                "content": "댓글 내용",
+                "parent_no": None,
+            }
+        }
 
 
 class CommentUpdate(BaseModel):
